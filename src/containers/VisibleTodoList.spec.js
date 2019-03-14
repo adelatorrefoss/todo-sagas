a47baceed7b544e8ec4461e45bfab62e29c5a602
Mario Sánchez <msanchez@trovit.com>
2019-03-14 12:11:41 +0100

    Add test to getVisibleTodos for completed and active todo filters

--- src/containers/VisibleTodoList.spec.js
+++ src/containers/VisibleTodoList.spec.js
@@ -4,7 +4,7 @@ import { VisibilityFilters } from "../actions";
 describe("VisibleTodoList", () => {
   describe("getVisibleTodos", () => {
     describe("All", () => {
-      test("Should retunr all Todo items on state", () => {
+      test("Should return all Todo items on state", () => {
         expect(getVisibleTodos([
           {
             text: 'Run the tests',
@@ -16,8 +16,8 @@ describe("VisibleTodoList", () => {
             id: 1
           }
         ], VisibilityFilters.SHOW_ALL)).toMatchObject([{ "completed": false, "id": 0, "text": "Run the tests" }, { "completed": false, "id": 1, "text": "Use Redux" }])
-      })
-      test("Should retunr all Todo items on state even when we have completed and uncompleted todos", () => {
+      });
+      test("Should return all Todo items on state even when we have completed and uncompleted todos", () => {
         expect(getVisibleTodos([
           {
             text: 'Run the tests',
@@ -29,11 +29,41 @@ describe("VisibleTodoList", () => {
             id: 1
           }
         ], VisibilityFilters.SHOW_ALL)).toMatchObject([{ "completed": false, "id": 0, "text": "Run the tests" }, { "completed": true, "id": 1, "text": "Use Redux" }])
+      });
+    });
+    describe("Completed", () => {
+      test("Should return only completed Todo items on state", () => {
+        expect(getVisibleTodos([
+          {
+            text: 'Run the tests',
+            completed: false,
+            id: 0
+          }, {
+            text: 'Use Redux',
+            completed: true,
+            id: 1
+          }
+        ], VisibilityFilters.SHOW_COMPLETED)).toMatchObject([{ "completed": true, "id": 1, "text": "Use Redux" }])
       })
     });
-    // describe("Completed", () => {
-    // });
-    // describe("Active", () => {
-    // });
+    describe("Active", () => {
+      test("Should return only active Todo items on state", () => {
+        expect(getVisibleTodos([
+          {
+            text: 'Run the tests',
+            completed: false,
+            id: 0
+          }, {
+            text: 'Use Redux',
+            completed: true,
+            id: 1
+          }
+        ], VisibilityFilters.SHOW_ACTIVE)).toMatchObject([{
+          text: 'Run the tests',
+          completed: false,
+          id: 0
+        }])
+      })
+    });
   });
 });
