36e2d675b82f57b1f44218ce0b17d43f036012f4
Mario Sánchez <msanchez@trovit.com>
2019-03-14 12:14:58 +0100

    Move getVisibleTodos logics to the future selector function

---
+++ src/selectors.js
@@ -0,0 +1,23 @@
+import { VisibilityFilters } from "./actions";
+
+export function todoSelector(state) {
+  return state.todos;
+}
+
+export function visibilityFilterSelector(state) {
+  return state.visibilityFilter;
+}
+
+export function visibleTodosSelector(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(t => t.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(t => !t.completed)
+    default:
+      throw new Error('Unknown filter: ' + filter)
+  }
+}
+
