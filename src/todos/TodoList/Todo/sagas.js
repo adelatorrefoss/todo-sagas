3ce1d21f0074036e61716e2adcd1a100ceca4308
Antonio de la Torre <antondelatorre@gmail.com>
2019-03-20 14:07:08 +0100

    Adapt data

--- src/todos/TodoList/Todo/sagas.js
+++ src/todos/TodoList/Todo/sagas.js
@@ -8,35 +8,31 @@ export const adaptMarvelDataToTodoItems = result => {
 
     console.log(result);
 
-    return [
-        {
-            text: 'from marvel Write the tests',
-            completed: false,
-            id: 0
-        },
-        {
-            text: 'from marvel Run the tests',
-            completed: false,
-            id: 1
+    return result.map(x => {
+            return {
+                id: x.id,
+                text: x.name,
+                completed: false,
+            };
         }
-    ];
+    );
 };
 
 export function* loadTodos() {
 
-  const result = yield call(api.getMarvelData);
+    const result = yield call(api.getMarvelData);
 
-  const data = yield call(adaptMarvelDataToTodoItems, result);
+    const data = yield call(adaptMarvelDataToTodoItems, result);
 
-  yield put(loadTodosAction(data));
+    yield put(loadTodosAction(data));
 }
 
 export function* watchLoadTodos() {
-  yield takeEvery('LOAD_TODOS_ASYNC', loadTodos)
+    yield takeEvery('LOAD_TODOS_ASYNC', loadTodos)
 }
 
 export default function* rootSaga() {
-  yield all([
-    watchLoadTodos(),
-  ]);
+    yield all([
+        watchLoadTodos(),
+    ]);
 }
